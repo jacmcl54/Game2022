6d88f2cdb53387d9e30091eed27e9b63eaa05a54
Jack McLeod <jacmcl54@kingshigh.school.nz>
2022-03-03 12:16:42 +1300

    [JUMPING!] New update!!

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -1,9 +1,17 @@
 extends KinematicBody2D
 
-var motion = Vector2()
+const UP = Vector2(0,-1)
 const SPEED = 100
+var gravity = 20
+var jump_height =-150
+var motion = Vector2()
 
-func movement():
+func _physics_process(delta):
+	motion = move_and_slide(motion, UP)
+
+func _process(delta):
+	motion.y += gravity
+	var jump = Input.is_action_pressed("ui_up")
 	var left = Input.is_action_pressed("ui_left")
 	var right = Input.is_action_pressed("ui_right")
 	
@@ -11,9 +19,11 @@ func movement():
 		motion.x = -SPEED
 	elif right:
 		motion.x = SPEED
+	elif jump:
+		if is_on_floor():
+			motion.y = jump_height
 	else:
 		motion.x = 0
 	motion = move_and_slide(motion)
 	
-func _physics_process(delta):
-	movement()
+
