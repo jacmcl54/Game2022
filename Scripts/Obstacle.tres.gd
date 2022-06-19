8bac7c256ea007be8133b026f1fea0681c635df4
calcap36 <82478261+calcap36@users.noreply.github.com>
2022-06-20 09:41:59 +1200

    speedofmushies

--- Scripts/Obstacle.tres.gd
+++ Scripts/Obstacle.tres.gd
@@ -2,3 +2,9 @@ extends "Scroolmovemnet.gd"
 
 func _physics_process(delta):
 	move()
+
+
+func _on_effectDamage_body_entered(body):
+	if body.name == "Player":
+		print("death")
+	
--- Scripts/item.gd
+++ Scripts/item.gd
@@ -2,3 +2,7 @@ extends "Scroolmovemnet.gd"
 
 func _physics_process(delta):
 	move() 
+
+
+func _on_pickup_body_entered(body):
+	print("player picked up berry")
