d66617741d0dd8e7a11621b8cc4755955c1d8750
Jack McLeod <jacmcl54@kingshigh.school.nz>
2022-07-07 09:55:10 +1200

    7/7/22

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -2,12 +2,14 @@ extends Control
 
 
 func _ready():
+	$AudioStreamPlayer2D.stop()
 	$Menu/CentreRow/Buttons/PlayGameButton.grab_focus()
 	for button in $Menu/CentreRow/Buttons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 	$AnimationPlayer.play("Fade in")
 	yield(get_tree().create_timer(3), "timeout")
 	$FadeIn.hide()
+	$AudioStreamPlayer2D.play()
 
 
 func _on_Button_pressed(scene_to_load):
