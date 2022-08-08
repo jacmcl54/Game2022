0d54d7d35336df87fa26ef9c96b038c0fa0a8a9c
Jack McLeod <jacmcl54@kingshigh.school.nz>
2022-08-09 10:19:30 +1200

    09/08/2022

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -7,10 +7,10 @@ func _ready():
 	for button in $Menu/CentreRow/Buttons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 	$AnimationPlayer.play("Fade in")
+	$AudioStreamPlayer2D.play()
 	yield(get_tree().create_timer(3), "timeout")
 	$FadeIn.hide()
-	$AudioStreamPlayer2D.play()
-
+	
 
 func _on_Button_pressed(scene_to_load):
 	get_tree().change_scene(scene_to_load)
